24a644c6ada22645b18f34c20df3978258d0703d
kstor001 <kastormer@ucdavis.edu>
2017-02-14 04:18:20 -0800

    Add files via upload

--- studio4/script.js
+++ studio4/script.js
@@ -3,12 +3,23 @@ console.log('reading');
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
-    var time = new Date();
-    console.log(
-        ("0" + time.getHours()).slice(-2) + ":" +
-        ("0" + time.getMinutes()).slice(-2) + ":" +
-        ("0" + time.getSeconds()).slice(-2));
+    (function() {
+        function checkTime(i) {
+            return (i < 10) ? "0" + i : i;
+        }
 
+        function startTime() {
+            var today = new Date(),
+                h = checkTime(today.getHours()),
+                m = checkTime(today.getMinutes()),
+                s = checkTime(today.getSeconds());
+            document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+            t = setTimeout(function() {
+                startTime()
+            }, 500);
+        }
+        startTime();
+    })();
 
     var elements = document.getElementsByTagName('li');
     var tipTimer, tipId;
