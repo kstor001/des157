982e17b2f0265a6a1c4f3aeadd0d6193621dcbdc
kstor001 <kastormer@ucdavis.edu>
2017-03-14 01:32:43 -0700

    attempt to fix proto3 problems

---
+++ project/process/proto3/script.js
@@ -0,0 +1,38 @@
+console.log("reading");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+
+ //onclick change screen from title to emotions
+    var title= document.getElementById("title").onclick = function (e) {
+      e.target.style.visibility = "hidden";
+      document.getElementById("emotionsAll").style.display = "block";
+    };
+
+    var vibrant= document.getElementById("vibrant").onclick = function () {
+      var x = document.getElementById ("vibrant");
+      var y = document.getElementById("emotionsAll");
+      var z =document.getElementById("vibe2");
+      var range=document.getElementById("range");
+      if (y.style.display === "block") {
+        y.style.display = "none";
+        z.style.display = "block";
+        range.style.display = "block";
+      }
+
+      function updateSlider(slideAmount) {
+        //get element
+        var display = document.getElementById ("chosen");
+        //show the amount
+        display.innerHTML=slideAmount;
+
+
+      //get the element
+      var pic = document.getElementById("pic");
+      //set the dimensions
+      pic.style.width=slideAmount+"%";
+      pic.style.height=slideAmount+"%";
+      }
+
+};
