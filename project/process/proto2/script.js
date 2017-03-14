3afe719340456b4051f186c27c42adba6bcc470a
kstor001 <kastormer@ucdavis.edu>
2017-03-14 11:47:13 -0700

    updated process and prototype2

--- project/process/proto2/script.js
+++ project/process/proto2/script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
  //onclick change screen from title to emotions
     var title= document.getElementById('title').onclick = function (e) {
+      console.log('clicked title');
       e.target.style.visibility = 'hidden';
       document.getElementById('emotionsAll').style.display = 'block';
     }
@@ -14,19 +15,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
       var x = document.getElementById ('vibrant');
       var y = document.getElementById('emotionsAll');
       var z =document.getElementById('vibe2');
-      var range=document.getElementById('range');
+      var slider=document.getElementById('slider');
+
       if (y.style.display === 'block') {
         y.style.display = 'none';
         z.style.display = 'block';
-        range.style.display = 'block';
+        slider.style.display = 'block';
       }
+    }
 
       function updateSlider(slideAmount) {
         //get element
         var display = document.getElementById ("chosen");
         //show the amount
         display.innerHTML=slideAmount;
-
 
       //get the element
       var pic = document.getElementById("pic");
