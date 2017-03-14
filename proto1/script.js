293670009bb839e25a64a1b6c0b4a6a44e68a008
kstor001 <kastormer@ucdavis.edu>
2017-03-14 08:22:23 -0700

    working prototype 1
    fix the other bugs later

--- proto1/script.js
+++ proto1/script.js
@@ -3,36 +3,21 @@ console.log('reading');
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
-    //get elements by ID and create variables
-    var title=document.getElementById ('title');
-    var emotionsAll=document.getElementByClass ('emotionsAll');
-    var prompt=document.getElementById ('prompt');
-    var vibrant=document.getElementById ('vibrant');
-    var anxious=document.getElementById ('anxious');
-    var indifferent=document.getElementById ('indifferent');
-    var depressed=document.getElementById ('depressed');
-    var agitated=document.getElementById ('agitated');
-    var other=document.getElementById ('other');
 
-    document.getElementById("title").onclick = function (e) {
+ //onclick change screen from title to emotions
+    var title= document.getElementById('title').onclick = function (e) {
       e.target.style.visibility = 'hidden';
-    };
-
-  /*  //create toggleContent() function
-    function toggleContent(){
-      //get variable
-      var title=document.getElementByClass ('title');
-
-      //get current display setting
-      var displaySetting = title.style.display;
+      document.getElementById('emotionsAll').style.display = 'block';
     }
 
-    //make title screen disappear when someone clicks anywhere on screen
-    if (displaySetting == 'block') {
-      //hide visible title
-      title.style.display='none';
-    } */
-
-
+    var emotionsAll=document.getElementById('emotionsAll').onclick = function () {
+      var x = document.getElementById('vibrant');
+      var y = document.getElementById('emotionsAll');
+      var z = document.getElementById('vibe2');
+      if(y.style.display === 'block') {
+        y.style.display = 'none';
+        z.style.display = 'block';
+      }
+    }
 
 });
